b6a2266c81940e5bd5254dd891523bbe9d261de3
Eduardo <eduardonp03@gmail.com>
2024-03-30 18:09:06 -0300

    Primeiro commit

--- App.js
+++ App.js
@@ -5,18 +5,18 @@ export default function App() {
   return (
     <View style={styles.form}>
       <View style={styles.boxLogo}>
-      <Image style={styles.logo} source={require('assets\logo-pra-fazer.png')}>
+      <Image style={styles.logo} source={require('./src/assets/logo-pra-fazer.png')}>
       </Image>
     </View>
     <View>
-        <Text style={styles.label}></Text>
+        <Text></Text>
         <TextInput style={styles.input} placeholder='E-mail'>
         </TextInput>
     </View>
     <View>
-        <Text style={styles.label}></Text>
-        <TextInput style={styles.input}
-        placeholder='Senha'></TextInput>
+        <Text></Text>
+        <TextInput style={styles.input}placeholder='Senha'>
+        </TextInput>
     </View>
     <TouchableOpacity style={styles.btn}
     >
@@ -33,61 +33,45 @@ export default function App() {
 const styles = StyleSheet.create({
     form: {
         backgroundColor: '#FF6600',
-        padding: 30,
         height: '100%',
-    },
-    label: {
-        fontSize: 18,
-        marginBottom: 10,
-        
+        alignItems: "center",
+        justifyContent: "center",
     },
     user:{
-      fontSize: 18,
-      marginBottom: 10,
-      textAlign: 'center',
-      color: '#FFF',
+        fontSize: 18,
+        marginBottom: 10,
+        textAlign: 'center',
+        color: '#FFF',
     },
     input: {
-        fontSize: 28,
+        fontSize: 18,
         borderRadius: 10,
         backgroundColor: '#DDD',
-        padding: 10,
-        marginBottom: 20,
+        padding: 25,
+        marginBottom: 10,
+        width: 500,
     },
     btn: {
         backgroundColor: '#070A52',
         padding: 10,
         borderRadius: 10,
         marginBottom: 20,
+        width: 500,
+        marginTop: 10,
     },
     btnText: {
         fontSize: 24,
         color: '#FFF',
         textAlign: 'center',
     },
-    media: {
-        fontSize: 40,
-        textAlign: 'center',
-        backgroundColor: '#DDD',
-        padding: 10,
-        borderRadius: 10,
-    },
-    resultado: {
-        fontSize: 24,
-        textAlign: 'center',
-        backgroundColor: '#32CD32',
-        color: '#FFF',
-        padding: 10,
-        borderRadius: 10,
-   },
     boxLogo: {
-        backgroundColor: '#09C',
-        marginBottom: 40,
+        marginBottom: 5,
         paddingTop: 80,
         alignItems: 'center',
   },
     logo: {
-        width: 260,
+        width: 280,
         height: 50,
+        alignItems: 'center'
   }
 });
